3348171d81bdcaf5391043aeda6f3ae54c241496
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-20 17:17:04 +0300

    handling requests

---
+++ app/api/login/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import { verifyPassword } from "@/helpers/hash";
+import { connectToDatabase } from "@/helpers/connectToDatabase";
+
+// This function handles POST requests for user login
+export async function POST(req: Request) {
+    // Parse the request body to JSON
+    const data = await req.json();
+
+    // Extract email and password from the request data
+    const { email, password } = data;
+
+    // Connect to the 'users' collection in the database
+    const collection = await connectToDatabase("users");
+
+    // Find a user document in the collection that matches the provided email
+    const filteredDocs = await collection.find({ email: email }).toArray();
+
+    // If no user is found, return a 401 status with a "User not found" message
+    if (filteredDocs.length === 0) {
+        return NextResponse.json({ status: 401, message: "User not found" });
+    }
+
+    // Verify the provided password against the stored password
+    const passwordIsValid = verifyPassword(filteredDocs[0].password, password);
+
+    // If the password is not valid, return a 500 status with a "Wrong password" message
+    if (!passwordIsValid) {
+        return NextResponse.json({
+            status: 500,
+            message: "Wrong password",
+        });
+    }
+
+    // If the user's email is not verified, return a 401 status with a verification message
+    if (!filteredDocs[0].verified) {
+        return NextResponse.json({
+            status: 401,
+            message: "Please verify your email address before logging in",
+        });
+    }
+
+    // If all checks pass, return a 200 status with a "Success" message
+    return NextResponse.json({ status: 200, message: "Success" });
+}
---
+++ app/api/signup/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { hashUserPassword } from "@/helpers/hash";
+import { connectToDatabase } from "@/helpers/connectToDatabase";
+
+// POST handler function
+export async function POST(req: Request) {
+    // Parsing the request body to JSON
+    const data = await req.json();
+
+    // Destructuring the data object to get username, email, and password
+    const { username, email, password } = data;
+
+    // Hashing the password
+    const hashedPassword = hashUserPassword(password);
+
+    // Connecting to the database
+    const collection = await connectToDatabase("users");
+
+    const filteredDocs = await collection.find({ email: email }).toArray();
+
+    if (filteredDocs.length > 0) {
+        return NextResponse.json({
+            status: 401,
+            message: "A user with this email already exists",
+        });
+    }
+
+    // Inserting a new document into the collection
+    await collection.insertOne({
+        username: username,
+        email: email,
+        password: hashedPassword,
+        verified: false,
+    });
+
+    // Sending a response back to the client
+    return NextResponse.json({ status: 200 });
+}
