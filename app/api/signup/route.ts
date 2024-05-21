caa3fb11ca8e9289228692aa8a7d4d5ac0601c38
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-21 17:20:44 +0300

    commit error handleling

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -42,15 +42,23 @@ export async function POST(req: Request) {
 
     const emailToken = uuidv4();
 
-    // If a user with the provided email doesn't exist, insert a new user into the collection
-    await collection.insertOne({
-        username: username,
-        email: email,
-        password: hashedPassword,
-        verified: false,
-        emailToken: emailToken,
-        timestamp: new Date(),
-    });
+    try {
+        // If a user with the provided email doesn't exist, insert a new user into the collection
+        await collection.insertOne({
+            username: username,
+            email: email,
+            password: hashedPassword,
+            verified: false,
+            emailToken: emailToken,
+            timestamp: new Date(),
+        });
+    } catch (error) {
+        // If an error occurs during the insertion, return a 500 status code
+        return NextResponse.json({
+            status: 500,
+            message: "Failed to insert user into the database",
+        });
+    }
 
     // Create the verification HTML using the imported createVerificationHTML function
     const html = createVerificationHTML(
@@ -58,8 +66,16 @@ export async function POST(req: Request) {
         `${API_URL}/verify-email?email=${email}&token=${emailToken}`
     );
 
-    // Send the verification email using the imported sendEmail function
-    sendEmail(email, "Verify your email", html);
+    try {
+        // Send the verification email using the imported sendEmail function
+        sendEmail(email, "Verify your email", html);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            status: 500,
+            message: "Failed to send verification email",
+        });
+    }
 
     // If everything goes well, return a 200 status code
     return NextResponse.json({ status: 200 });
