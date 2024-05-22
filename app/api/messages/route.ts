90fd87b78f9aa1282a7376b154e91ef59c5d2fad
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-22 16:15:20 +0300

    chore: Update MessagesPage to fetch and display messages from API

---
+++ app/api/messages/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { connectToDatabase } from "@/helpers/connectToDatabase";
+
+// This function handles GET requests
+export async function GET() {
+    // Connect to the 'users' collection in the database
+    const collection = await connectToDatabase("messages");
+
+    if (!collection) {
+        return NextResponse.json({
+            status: 500,
+            message: "Failed to connect to the database serve",
+        });
+    }
+
+    // Find a user document in the collection that matches the provided email
+    const filteredDocs = await collection.find({ mode: "sample" }).toArray();
+
+    const messages = filteredDocs[0].messages;
+
+    // If all checks pass, return a 200 status with a "Success" message
+    return NextResponse.json({
+        status: 200,
+        message: {
+            messages,
+        },
+    });
+}
--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { isUserLoggedIn } from "@/helpers/isUserLoggedIn";
 import { logOut } from "@/actions/authActions";
-import { cookies } from "next/headers";
+import { API_URL } from "@/constants";
 
 export default async function MessagesPage() {
     const user = await isUserLoggedIn();
@@ -12,9 +12,20 @@ export default async function MessagesPage() {
         redirect("/login");
     }
 
+    const response = await fetch(`${API_URL}/api/messages`);
+    const data = await response.json();
+
+    const messages = data.message.messages;
+    console.log(messages);
     return (
         <>
-            <div>Messages</div>
+            <div>
+                {messages
+                    ? messages.map((message: string) => {
+                          return <div key={message}>{message}</div>;
+                      })
+                    : "loading..."}
+            </div>
             <form action={logOut}>
                 <button type="submit">Logout</button>
             </form>
